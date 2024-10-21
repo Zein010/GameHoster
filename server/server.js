12ac80d67c4844dc1bfea9fa22f61564700af01e
root <root@Zyxn-PC>
2024-10-21 12:33:28 +0300

    wip

--- server/server.js
+++ server/server.js
@@ -14,11 +14,12 @@ app.get("/CreateServer", async (req, res) => {
   await TerminalService.CreateNewDirectory({ name: dirName })
   await TerminalService.CreateUser(username);
   const scriptFile = await TerminalService.DownloadServerData(gameVersion.downloadLink, dirName);
+  console.log(gameVersion);
   await TerminalService.SetupRequiredFiles(dirName, gameVersion.getFilesSetup)
   await TerminalService.OwnFile(dirName, username)
   await TerminalService.RunGameServer(dirName, scriptFile, username, gameVersion, true)
   await TerminalService.SetupServerAfterStart(dirName, gameVersion.changeFileAfterSetup);
-  res.json({ msg: "Game server create successfully" });
+  res.json({ msg: "Game server created successfully" });
 })
 app.listen(3000, () => {
   console.log("Server started on port 3000");
