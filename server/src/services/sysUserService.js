4ead833b1b488e75d80722827aa41c29a285a091
root <root@Zyxn-PC>
2024-10-21 22:58:53 +0300

    wip

--- server/src/services/sysUserService.js
+++ server/src/services/sysUserService.js
@@ -13,7 +13,7 @@ const Get = async (id) => {
     return await prisma.sysUser.findUnique({ where: { id }, include: { userAccess: true, runningServers: true } })
 }
 const DeleteUser = async (id) => {
-    return await prisma.sysUser.update({ where: { id }, data: { delete: true, deletedAt: new Date() } })
+    return await prisma.sysUser.update({ where: { id }, data: { deleted: true, deletedAt: new Date() } })
 }
 const sysUserService = { StoreSysUser, GetAll, Get, DeleteUser };
 export default sysUserService
