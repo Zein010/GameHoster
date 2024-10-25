074bcafa4f6a61b93ec2f14f6540501c909cfe6e
root <root@Zyxn-PC>
2024-10-25 18:56:24 +0300

    wip

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-const apiUrl = import.meta.env
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()], server: { host: "zyxnware.com" }
-})
+
+
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (e.g., 'development' or 'production')
+  const env = loadEnv(mode, process.cwd());
+
+  console.log(env.VITE_HOST);  // This should print the correct value
+
+  return {
+    server: {
+      host: env.VITE_HOST,
+      port: parseInt(env.VITE_PORT, 10),
+    },
+    plugins: [react()],
+  };
+});
