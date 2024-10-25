8d71e502b4986deef0b07ce5f788ef2eda54c67d
root <root@Zyxn-PC>
2024-10-25 18:44:04 +0300

    wip

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+const apiUrl = import.meta.env
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], server: { host: "zyxnware.com" }
 })
